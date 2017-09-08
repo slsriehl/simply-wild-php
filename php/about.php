f8444d261dcd196f9e27d176aa1358f9d2501851
Sarah Riehl <sls.riehl@gmail.com>
2017-09-08 13:04:26 -0500

    finish header html and styles

---
+++ php/about.php
@@ -0,0 +1,5 @@
+<div class="section" id="about">
+	<div class="section__jumbo section--img" id="about--img">
+	</div>
+<!-- /section -->
+</div>
--- php/header.php
+++ php/header.php
@@ -1,59 +1,71 @@
 <div class="header">
-	<div class="header__menu">
-		<div class="header__menu__contact">
-			<i class="fa fa-bars header__menu__contact__text menu--icon" id="header__menu__toggle">
-			</i>
-			<a href="tel:+15129707776" class="header__menu__contact__text menu--phone">
-				512 970 7776
-			</a>
-			<a href="https://facebook.com/SimplyWildGardens" target="_blank" class="fa fa-facebook-square header__menu__contact__text menu--icon">
-			</a>
-		<!-- /.header__menu__contact -->
+	<div class="header__nav">
+		<div class="header__menu">
+			<div class="header__menu__contact">
+				<i class="fa fa-bars header__menu__contact__text menu--icon" id="header__menu__toggle">
+				</i>
+				<a href="tel:+15129707776" class="header__menu__contact__text menu--phone">
+					512 970 7776
+				</a>
+				<a href="https://facebook.com/SimplyWildGardens" target="_blank" class="fa fa-facebook-square header__menu__contact__text menu--icon">
+				</a>
+			<!-- /.header__menu__contact -->
+			</div>
+			<ul class="header__menu__hmm">
+				<li class="hmm__list menu--item">
+					<a href="#about">
+						<span>
+							ABOUT
+						</span>
+					</a>
+				</li>
+				<li class="hmm__list menu--item">
+					<a href="#services">
+						<span>
+							SERVICES
+						</span>
+					</a>
+				</li>
+				<li class="hmm__list menu--item">
+					<a href="#gardens">
+						<span>
+							GARDENS
+						</span>
+					</a>
+				</li>
+				<li class="hmm__list menu--item">
+					<a href="#comments">
+						<span>
+							COMMENTS
+						</span>
+					</a>
+				</li>
+				<li class="hmm__list menu--item">
+					<a href="#contact">
+						<span>
+							CONTACT
+						</span>
+					</a>
+				</li>
+			<!-- /.header__menu__menu -->
+			</ul>
+		<!-- /.header__menu -->
 		</div>
-		<ul class="header__menu__hmm">
-			<li class="hmm__list menu--item">
-				<a href="#about">
-					<span>
-						ABOUT
-					</span>
-				</a>
-			</li>
-			<li class="hmm__list menu--item">
-				<a href="#services">
-					<span>
-						SERVICES
-					</span>
-				</a>
-			</li>
-			<li class="hmm__list menu--item">
-				<a href="#gardens">
-					<span>
-						GARDENS
-					</span>
-				</a>
-			</li>
-			<li class="hmm__list menu--item">
-				<a href="#comments">
-					<span>
-						COMMENTS
-					</span>
-				</a>
-			</li>
-			<li class="hmm__list menu--item">
-				<a href="#contact">
-					<span>
-						CONTACT
-					</span>
-				</a>
-			</li>
-		<!-- /.header__menu__menu -->
-		</ul>
-	<!-- /.header__menu -->
+		<div class="header__logo">
+			<div class="logo--img">
+			</div>
+			<!-- /.header__logo -->
+		</div>
+	<!-- /.header__nav -->
 	</div>
-	<div class="header__logo">
-		<div class="logo--img">
-		</div>
-		<!-- /.header__logo -->
+	<div class="header__services">
+		<span>PERSONALIZED SERVICES FOR YOUR GARDEN:&nbsp;</span>
+		<span>CONSULTATION&nbsp;&bull;&nbsp;DESIGN</span>
+		<span class="header__services--bullet">&nbsp;&bull;&nbsp;</span>
+		<span>INSTALLATION&nbsp;&bull;&nbsp;GROOMING</span>
+		<span class="header__services--bullet">&nbsp;&bull;&nbsp;</span>
+		<span>MAINTENANCE</span>
+	<!-- /.header__services -->
 	</div>
 <!-- /.header -->
 </div>
