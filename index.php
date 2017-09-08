9552140f1856402309499fef4d76f1808a7ce0a2
Sarah Riehl <sls.riehl@gmail.com>
2017-09-07 21:37:54 -0500

    initial php files and header.php initial build

---
+++ index.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<!--[if lte IE 6]><html class="preIE7 preIE8 preIE9" lang="en"><![endif]-->
+<!--[if IE 7]><html class="preIE8 preIE9" lang="en"><![endif]-->
+<!--[if IE 8]><html class="preIE9" lang="en"><![endif]-->
+<!--[if gte IE 9]><!--><html lang="en"><!--<![endif]-->
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<meta name="viewport" content="width=device-width,initial-scale=1" />
+			<title>Simply Wild Gardens</title>
+		<meta name="author" content="Sarah Schieffer Riehl" />
+		<meta name="description" content="Austin garden design small business" />
+		<meta name="keywords" content="Austin garden design small business" />
+		<link rel="shortcut icon" href="public/img/dragonfly.png" type="image/png" />
+		<link rel="stylesheet" href="src/css/styles.css" type="text/css" />
+		<link rel="stylesheet" href="src/css/styles.css" type="text/css" />
+	</head>
+	<body>
+		<?php
+			include './php/header.php';
+			include './php/about.php';
+			include './php/services.php';
+			include './php/gardens.php';
+			include './php/comments.php';
+			include './php/contact.php';
+			include './php/footer.php';
+		?>
+	</body>
+</html>
