723372ac2b19a481d5863f659c4885d916284213
Sarah Riehl <sls.riehl@gmail.com>
2017-09-08 18:21:59 -0500

    services page

---
+++ php/services.php
@@ -0,0 +1,49 @@
+<div class="section" id="services">
+	<div class="section__jumbo section--img" id="services--img">
+	<!-- /section__jumbo -->
+	</div>
+	<div class="section__text">
+		<p class="section__p">
+			<span class="section__span--tagline">
+				Garden services tailored to your needs.&nbsp;
+			</span>
+			We can design an easy-care garden you can enjoy year 'round.
+		</p>
+    <p class="section__p section__span--tagline">
+			We can do it all:
+		</p>
+    <ul class="section--bullets">
+      <li>
+				Select drought-hardy and native plants to attract wildlife
+      </li>
+      <li>
+				Prepare your soil
+      </li>
+      <li>
+				Install plants
+      </li>
+      <li>
+				Maintain your garden
+			</li>
+    </ul>
+    <p class="section__p section__span--tagline">
+			Or we can come coach you so you can do it yourself:
+		</p>
+    <ul class="section--bullets">
+      <li>
+				Advise on selecting Texas tough plants for a wildlife habitat
+      </li>
+      <li>
+				Teach you to maintain your garden including no-fuss composting and leaf mulching
+      </li>
+      <li>
+				Show you how to control garden pests organically
+      </li>
+      <li>
+				Offer tips and tricks for easy gardening
+      </li>
+    </ul>
+	<!-- /section__text -->
+	</div>
+<!-- /section -->
+</div>
