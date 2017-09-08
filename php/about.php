40df1bc380b67041cbd52e5c3665ddea6ae9760b
Sarah Riehl <sls.riehl@gmail.com>
2017-09-08 18:05:10 -0500

    section, about styles

--- php/about.php
+++ php/about.php
@@ -1,5 +1,26 @@
 <div class="section" id="about">
 	<div class="section__jumbo section--img" id="about--img">
+	<!-- /section__jumbo -->
+	</div>
+	<div class="section__text">
+		<p class="section__p">
+			<span class="section__span--tagline">
+				Start something wild!&nbsp;
+			</span>
+			Let us create a habitat garden retreat to attract birds, butterflies, and other wildlife for you to enjoy.  We can consult with you to design a space, and install and maintain your organic, environmentally friendly wildlife garden that works for you, your family, and your home or business.
+		</p>
+		<div class="section__side__img" id="suzi--img">
+		</div>
+		<p class="section__p">
+			<span class="section__span--tagline">
+				What we do:&nbsp;
+			</span>
+			 We combine our loves of wildlife, good design, and natural gardening to create habitat garden retreats that delight all the senses.  Our landscapes are water-wise and well adapted to the whimsical weather of Central Texas without sacrificing curb appeal.  Founder Suzi Sands has been Native Plant Certified through the Lady Bird Johnson Wildflower Center and has completed studies at Austin's Grow Green Sustainable Landscaping.  She will work with you to create the space you have always wanted and help urban Austin go wild at the same time.&nbsp;
+			<span class="section__span--tagline">
+				Take a walk on the wild side in your garden.
+			</span>
+		</p>
+	<!-- /section__text -->
 	</div>
 <!-- /section -->
 </div>
