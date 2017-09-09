355bd03b00ef3a042f03383f91bea832befb21e2
Sarah Riehl <sls.riehl@gmail.com>
2017-09-08 19:17:05 -0500

    gardens

---
+++ php/gardens.php
@@ -0,0 +1,28 @@
+<div class="section" id="gardens">
+	<div class="garden__image__row">
+		<div class="garden__image" id="garden__image--1">
+		</div>
+		<div class="garden__image" id="garden__image--2">
+		</div>
+		<div class="garden__image" id="garden__image--3">
+		</div>
+	<!-- / .garden__image__row -->
+	</div>
+
+	<div class="header__services" id="garden--text">
+		<span>RELAX ON A BEAUTIFUL DAY</span>
+	<!-- /.header__services -->
+	</div>
+
+	<div class="garden__image__row">
+		<div class="garden__image" id="garden__image--4">
+		</div>
+		<div class="garden__image" id="garden__image--5">
+		</div>
+		<div class="garden__image" id="garden__image--6">
+		</div>
+	<!-- / .garden__image__row -->
+	</div>
+
+<!-- /section -->
+</div>
