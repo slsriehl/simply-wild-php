4ddf74558d4ce18a72aeb44a62038c99498966b9
Sarah Riehl <sls.riehl@gmail.com>
2017-09-08 21:25:33 -0500

    footer html and send-mail file creation

---
+++ php/footer.php
@@ -0,0 +1,14 @@
+<div class="footer__container">
+<span class="footer--span">
+	<span class="footer--copy">
+		&copy;&nbsp;
+		<span id="footer--copyright"></span>
+	</span>
+	&nbsp;
+	<a href="https://studioriehl.com" target="_blank">
+		Studio Riehl
+	</a>
+	with Simply Wild Gardens
+</span>
+<!-- / .footer__container -->
+</div>
