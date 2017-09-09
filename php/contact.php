f07db71fe8116842f848a99cfd082c9682d1c24d
Sarah Riehl <sls.riehl@gmail.com>
2017-09-08 21:22:46 -0500

    mail form html and scss

---
+++ php/contact.php
@@ -0,0 +1,51 @@
+<div class="section" id="contact">
+	<div class="section__jumbo section--img" id="contact--img">
+	<!-- /section__jumbo -->
+	</div>
+	<div class="section__text section__form">
+		<p class="section__p">
+			<span class="section__span--tagline" id="replaceOnSubmit">
+				Give us a shout!&nbsp;
+			</span>
+			<span id="hideOnSubmit">You can email us using the form below or c</span><span id="showOnSubmit">C</span>all or text us at <a href="tel:+15129707776" class="section__span--tagline">512&nbsp;970&nbsp;7776</a>.  You can also like us on
+			<a href="http://www.facebook.com/SimplyWildGardens" class="section__span--tagline">
+				Facebook
+			</a>
+			.
+		</p>
+		<form class="contact__form" id="emailSuzy">
+				<div class="contact__float-label">
+					<input class="contact__input" name="name" type="text"/>
+					<label for="name">Name</label>
+				</div>
+
+				<div class="contact__float-label">
+					<input class="contact__input" name="email" type="email" />
+					<label for="email">Email</label>
+				</div>
+
+			<label class="contact__float-label">
+				<input class="contact__input" name="tel" type="tel" />
+				<label for="tel">Phone (optional)</label>
+			</label>
+
+			<label class="contact__float-label">
+				<input class="contact__input" name="subject" type="text" />
+				<label for="subject">Subject</label>
+			</label>
+
+			<label class="contact__float-label">
+				<textarea class="contact__input" name="message" rows="30"></textarea>
+				<label for="message">Message</label>
+			</label>
+
+			<div class="contact__button__container">
+				<button type="submit" class="contact__button">
+					Send
+				</button>
+			</div>
+		</form>
+	<!-- /section__text -->
+	</div>
+<!-- /section -->
+</div>
