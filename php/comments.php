a17b6af4ba8e05c4cfd6145968030225a6dff862
Sarah Riehl <sls.riehl@gmail.com>
2017-09-08 21:23:07 -0500

    html for comments section

---
+++ php/comments.php
@@ -0,0 +1,36 @@
+<div class="section" id="comments">
+	<div class="section__jumbo section--img" id="comments--img">
+	<!-- /section__jumbo -->
+	</div>
+	<div class="section__text">
+			<p class="section__p">
+				"[Suzi] is fun to work with and has great ideas.  I will continue to use her whenever I can."
+				<span class="section__span--tagline">
+					&nbsp;&nbsp;Kaye
+				</span>
+			</p>
+			<p class="section__p">
+				"Suzi created 'rooms' in my backyard and made it so inviting that it is my favorite place to be.  I sit in the 'Sanctuary' room with a good book and good wine and watch the birds, the flowers, the light changing on the landscape, and feel completely at peace."
+				<span class="section__span--tagline">
+					&nbsp;&nbsp;Saralee
+				</span>
+			</p>
+			<p class="section__p">
+				"Suzi rose to the challenge of adding drought-hardy plants to an existing formal design and created a fresh, welcoming look."
+				<span class="section__span--tagline">
+					&nbsp;&nbsp;Betty
+				</span>
+			</p>
+			<p class="section__p">
+				<span class="section__span--tagline">
+					<a href="#contact" class="section__span--tagline">
+						Contact us
+					</a>
+					 for more testimonials and references.
+				</span>
+			</p>
+
+	<!-- /section__text -->
+	</div>
+<!-- /section -->
+</div>
